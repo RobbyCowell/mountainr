4e2c4a5e5ee2a709621e2f08ada28b54dd2411c1
Robby Cowell <hi@rocoweb.com>
2018-03-10 19:07:29 -0500

    added basic resort listing

--- src/components/elements/ResortCard.js
+++ src/components/elements/ResortCard.js
@@ -1,5 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class ResortCard extends Component {
-    
+    render() {
+        return (
+            <div className="resort-card">
+                <div classnme="row">
+                    <div className="col">
+                        <h3>{this.props.data.NAME}</h3>
+                        {/* <p>{this.props.usState}</p> */}
+                        <div className="resort-card__stats">
+                            <span>{this.props.data.LIFTS} lifts</span>
+                            <span>{this.props.data.TRAILS} trails</span>
+                            <span>{this.props.data.ACRES} acres</span>
+                            <span>Terrain park?{this.props.data.TERRAIN_PARK}</span>
+                        </div>
+                        <a href="#">Explore</a>
+                    </div>
+                    <div className="col">
+                        {/* <img src={this.props.logoUrl}/> */}
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
+
+export default connect()(ResortCard);
--- src/components/screens/Listing.js
+++ src/components/screens/Listing.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ResortCard from '../elements/ResortCard';
 
+// @connect((store) => { return { resorts: store.resorts }; })
 class Listing extends Component {
     render() {
+        let resortList = [];
+
+        for (var i = 0; i < this.props.resorts.length; i++) {
+            resortList.push(
+                <ResortCard
+                    key={i}
+                    history={this.props.history}
+                    data={this.props.resorts[i]}
+                />
+            );
+        }
+
         return (
-            <div>
-                <h1>Listing</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col text-center">
+                        <h1>Listing</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    {resortList}
+                </div>             
             </div>
         )
     }
 }
 
-export default Listing;
+const mapStateToProps = store => {
+    return {
+      resorts: store.resorts
+    }
+  }
+
+export default connect(mapStateToProps)(Listing);
