a9251653d7846fd1631a42e15cede2cc303cdf05
Robby Cowell <hi@rocoweb.com>
2018-03-11 18:01:48 -0400

    added map and began styling details page

--- src/components/elements/ResortCard.js
+++ src/components/elements/ResortCard.js
@@ -12,7 +12,6 @@ class ResortCard extends Component {
             <div className="resort-card">
                 <div className="resort-card__info">
                     <h3>{this.props.data.name}</h3>
-                    {/* <p>{this.props.usState}</p> */}
                     <div className="resort-card__stats">
                         <span>{this.props.data.lifts} lifts</span>
                         <span>{this.props.data.trails} trails</span>
--- src/components/screens/Details.js
+++ src/components/screens/Details.js
@@ -1,34 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './Details.css';
 
 class Details extends Component {
     render() {
-
         let resortData = this.props.resorts.find(resort => resort.name === this.props.match.params.id);
-        console.log(resortData);
+
+        let terrainPark = "";
+        if (resortData.hasTerrainPark === 'true') {
+            terrainPark = <span>Terrain Park</span>
+        }
+
+        let encodedAddress =  encodeURIComponent(resortData.address.trim());
+        let mapSrc = "https://www.google.com/maps/embed/v1/place?q="
+            + encodedAddress +
+            "&key=AIzaSyAi6C3hi1lszSKxvUvxIR3GjSsHC1CpZfo" +
+            "&zoom=10";
+        let directionsSrc = "https://www.google.com/maps/dir/?api=1&destination=" + encodedAddress;
 
         return (
             <div>
-                <div className="container">
-                    <img src={resortData.logoUrl} />
-                    <h1>{this.props.match.params.id}</h1>
-                    <div>
-                        <span>{resortData.lifts} lifts</span>
-                        <span>{resortData.trails} trails</span>
-                        <span>{resortData.acres} acres</span>
-                        <span>Terrain park?{resortData.hasTerrainPark}</span>
+                <div className="text-center container">
+                    <img className="details__logo" src={resortData.logoUrl} />
+                    <h1 className="details__name">{this.props.match.params.id}</h1>
+                    <div className="details__stats">
+                        <span><b>{resortData.lifts}</b> lifts</span>
+                        <span><b>{resortData.trails}</b> trails</span>
+                        <span><b>{resortData.acres}</b> acres</span>
+                        {terrainPark}
                     </div>
                 </div>
-                <div className="container-fluid">
-                    <map></map>
+                <div className="details__map-container container-fluid">
+                <iframe className="details__map" frameBorder="0" src={mapSrc} allowFullScreen></iframe>
                 </div>
                 <div className="container">
                     <div className="row">
-                        <div className="detail__action col">
-                            <a href="#">Directions</a>
+                        <div className="text-center col">
+                            <a className="details__action" href={directionsSrc} target="_blank" rel="noopener">Directions</a>
                         </div>
-                        <div className="detail__action col">
-                            <a href={resortData.trailMapUrl} target="_blank" rel="noopener">Trail map</a>
+                        <div className="text-center col">
+                            <a className="details__action" href={resortData.trailMapUrl} target="_blank" rel="noopener">Trail map</a>
                         </div>
                     </div>
                     <div className="row">
